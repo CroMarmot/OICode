6d160b49ed9ac8c28d7aeb4ea916d31c62d8c875
cromarmot <yexiaorain@gmail.com>
2022-07-19 21:31:00 +0800

    update inv and bionm

--- functions/inv.cpp
+++ functions/inv.cpp
@@ -1,22 +1,45 @@
 #include<iostream>
 using namespace std;
+typedef long long ll;
 #define rep(i,a,n) for (int i=a;i<n;i++)
 
-namespace INV{
-  // 1-index
-  // [1->_arrlen]
-  void init_arr(long long *_arr,int _arrlen,int _mod){
-  _arr[1] = 1;
-  rep(i,2,_arrlen+1)
-    _arr[i] = ((_mod-_mod/i)*_arr[_mod%i])%_mod;
-  }
+ll read(){ll r;scanf("%lld",&r);return r;} // read
+int n;
+int MOD;
+
+struct ModInt{
+  int v;
+  ModInt(ll val = 0) :v(val) { }
+};
+
+ll real(const ModInt & v0){
+  return (v0.v + MOD) % MOD;
 }
 
-long long inv[100000];
+ModInt operator+(const ModInt & v0, const ModInt &v1){
+  return (v0.v + v1.v) % MOD;
+}
+
+ModInt operator-(const ModInt & v0, const ModInt &v1){
+  return (v0.v - v1.v) % MOD;
+}
+
+ModInt operator*(const ModInt & v0, const ModInt &v1){
+  return ((ll)v0.v * (ll)v1.v) % MOD;
+}
+
+ModInt fac[2010] = {1};
+ModInt invv[2010] = {1};
+ModInt invfac[2010] = {1};
+ModInt C(int n,int m) { return fac[n] * invfac[m] * invfac[n-m]; }
 
 int main(){
-  INV::init_arr(inv,1000,1'000'000'007);
-  rep(i,0,100){
-    cout<<i<<":"<<inv[i]<<endl;
-  }
+  n = read();
+  MOD = read();
+
+  // 30 ms
+  rep(i,1,n+1) fac[i] = fac[i-1] * i;
+  invv[1] = 1;
+  rep(i,2,n+1) invv[i] = (MOD-MOD/i) * invv[MOD%i];
+  rep(i,1,n+1) invfac[i] = invfac[i-1] * invv[i];
 }
