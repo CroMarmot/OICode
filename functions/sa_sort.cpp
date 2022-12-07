8fbb51516b4ac3952f18083af6e181e8d403f006
cromarmot <yexiaorain@gmail.com>
2022-12-08 01:56:27 +0800

    update by script

--- functions/sa_sort.cpp
+++ functions/sa_sort.cpp
@@ -10,22 +10,19 @@ typedef long long ll;
 template<class T>
 void calc_sa_rank(vector<T>& arr, vector<int> &sa,vector<int> &rank, vector<int>&h){
   int n = arr.size();
-  rank = vector<int>(n,0);
-  sa = vector<int>(n,0);
-  iota(sa.begin(),sa.end(), 0);
-  sort(sa.begin(),sa.end(), [&](int i,int j){return arr[i] < arr[j];});
-  rep(i,1,n) rank[sa[i]] = rank[sa[i-1]] + !(arr[sa[i]] == arr[sa[i-1]]);
-  for(int w = 1; w < n; w *= 2) {
-    auto rank0 = rank;
-    auto rk = [&](int i){return i < n ? rank0[i] : -1;};
-    sort(sa.begin(),sa.end(), [&](int i,int j){
-        return rk(i) != rk(j) ? rk(i) < rk(j) : rk(i+w) < rk(j+w);
-    });
+  rank = vector<int>(n);
+  sa = vector<int>(n);
+  iota(sa.begin(),sa.end(),0);
+  rep(i,0,n) rank[i]=arr[i];
+  for(int w=1; w<=n; w*=2) {
+    vector<pair<int,int>>val;
+    rep(i,0,n)val.push_back({rank[i],i+w/2<n?rank[i+w/2]:-1});
+    sort(sa.begin(),sa.end(), [&](int i,int j){ return val[i]<val[j]; });
     rank[sa[0]] = 0;
-    rep(i,1,n) rank[sa[i]] = rank[sa[i-1]] + !(rk(sa[i]) == rk(sa[i-1]) && rk(sa[i]+w) == rk(sa[i-1]+w));
+    rep(i,1,n) rank[sa[i]] = rank[sa[i-1]] + (val[sa[i]]!=val[sa[i-1]]);
   }
   // height
-  h = vector<int>(n,0);
+  h = vector<int>(n);
   int k = 0;
   rep(i,0,n) {
     if (rank[i] == 0) continue;
@@ -33,6 +30,21 @@ void calc_sa_rank(vector<T>& arr, vector<int> &sa,vector<int> &rank, vector<int>
     while (arr[i + k] == arr[sa[rank[i] - 1] + k]) ++k;
     h[rank[i]] = k;
   }
+}
+
+void auto_test() {
+  char s[100] = "aabaaaab";
+  int n = strlen(s) ;
+  printf("s:%s\n",s);
+  vector<char> arr ;
+  rep(i,0,n) arr.push_back(s[i]);
+  vector<int> sa;
+  vector<int> rank;
+  vector<int> h;
+  calc_sa_rank(arr, sa, rank, h);
+  assert(sa==(vector<int>{3,4,5,0,6,1,7,2}));
+  assert(rank==(vector<int>{3,5,7,0,1,2,4,6}));
+  assert(h==(vector<int>{0,3,2,3,1,2,0,1}));
 }
 
 
@@ -53,5 +65,6 @@ int main() {
   printf("\nhei:");
   rep(i,0,n) printf("%d ", h[i]);
   printf("\n");
+  auto_test();
   return 0;
 }
